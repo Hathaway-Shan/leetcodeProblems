07196c8837ac6aae43507f3dedf35bea79980333
Hathaway-Shan <hathaway.shan@gmail.com>
2023-01-13 14:43:52 -0800

    added math.abs solution to find disappearing nums

--- easy/Find Disappeared Numbers/findDisappearedNums.js
+++ easy/Find Disappeared Numbers/findDisappearedNums.js
@@ -65,36 +65,72 @@ The actual solution is meant to us Math.abs()
 Syntax: Math.abs(x)
 Returns: The absolute value of x. If x is negative (including -0), returns -x. 
 Otherwise, returns x. The result is therefore always a positive number or 0.
-
-Specifically Math.abs() will return a negative value if it operates on a negative number
-But if it operates on just a given number it will return positive
-Using this we can filter the array by marking numbers that exist negative
 */
 
 function findDisappearedNumAbs(array) {
-  const result = [];
-  console.log("line 76", array);
-  array.forEach((number) => {
-    const indexBasedOnThisValue = Math.abs(number) - 1;
-
-    if (array[indexBasedOnThisValue] > 0) {
-      array[indexBasedOnThisValue] = array[indexBasedOnThisValue] * -1;
-    }
-    console.log("line 83", array);
-  });
-
-  // Now that we've marked the array with negative numbers, loop through it again.
+  const returnArr = [];
 
   for (let i = 0; i < array.length; i++) {
-    // Check if the number at this position is positive or negative.
-    // Since arrays are 0-indexed, we'll be off by one, so add 1 when we push to results.
+    //to get values for all the indexs in our array so we can operate on their contents
+    const absoluteIndex = Math.abs(array[i]) - 1; //minus one to account for zero indexing
+
+    /*
+    console.log(absoluteIndex)
+    returns:
+    3 
+    2
+    1
+    6
+    7
+    1 
+    2 
+    0
+    */
+
+    if (array[absoluteIndex] > 0) {
+      /*
+        inside this conditional we mark the duplicate index by changing all values 
+        based on absolute index to negative numbers
+        any index operated on twice like 1 & 2 above will be operated on twice 
+        which turns the value positive
+        */
+      array[absoluteIndex] = array[absoluteIndex] * -1;
+      /*
+        3 testArr[3] = 7 becomes -7
+        2 testArr[2] = 2 becomes -2
+        1 testArr[1] = 3 becomes -3
+        6 testArr[6] = 3 becomes -3
+        7 testArr[7] = 1 becomes -1
+        1 testArr[1] = -3 does not enter conditional
+        2 testArr[2] = -2 does not enter conditional
+        0 testArr[0] = 4 becomes -4
+        
+        **noteably since line 36 and 37 are duplicates aka their number is missing
+        their contents are never operated on thus never set to negative
+        
+        testArr[4] is still 8
+        testArr[5] is still 2
+        */
+    }
+    /*
+    console.log(testArr)
+    returns:
+    [-4,-3,-2,-7,8,2,-3,-1]
+    when all operations complete
+    */
+  }
+  /*
+Now that numbers we operated on are marked negative we loop through the array again
+checking for positive values and returning their indexes
+*/
+
+  for (let i = 0; i < array.length; i++) {
     if (array[i] > 0) {
-      result.push(i + 1);
+      //remember to the correct index is at i + 1 because of zero indexing
+      console.log(`missing number found at index ${i + 1}`); // returns 5 & 6
+      returnArr.push(i + 1);
     }
   }
-
-  return result;
+  return returnArr;
 }
 console.log(findDisappearedNumAbs(nums));
-
-//I don't understand this solution at all I'll work on it
