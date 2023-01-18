9a24b7d64bcf86d93da937d280785ec9302aca8f
Hathaway-Shan <hathaway.shan@gmail.com>
2023-01-18 15:14:19 -0800

    added an additional solve to Majority Element

--- easy/Majority Element/majorityElement.js
+++ easy/Majority Element/majorityElement.js
@@ -37,3 +37,34 @@ function majorityElement(array) {
 }
 
 console.log(majorityElement(nums)); //returns expected
+
+/*
+This can also be solved in 0(n) time with a single pass for loop
+and no hashmap
+It is slightly less performant than the above
+this method: 71ms speed 43.8mb memory 80% 58%
+above method: 69ms speed 43.3 memory 84% 81%
+*/
+
+function majorityElementLoop(array) {
+  let majority;
+  let majorityCount = 0;
+
+  for (const number of array) {
+    //this starts the code and resets the value of number whenever majorityCount is equal to 0
+    if (majorityCount === 0) {
+      majority = number;
+      //the first time we set a new number we increment
+      majorityCount++;
+      //if the next number matches we increment again
+    } else if (majority === number) {
+      majorityCount++;
+    } else {
+      //if the next number doesn't match we decrement
+      //if this brings majority count to 0 we reset the value of majority to the number of the current index
+      majorityCount--;
+    }
+  }
+  //we return the last of majority with a positive majorityCount value
+  return majority;
+}
