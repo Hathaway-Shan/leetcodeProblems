9af5816625ec5e7366bb9e2a17a3d67734536c19
Hathaway-Shan <hathaway.shan@gmail.com>
2023-01-26 12:21:22 -0800

    added iterative solve for Reverse a Linked List

--- easy/Reverse a Linked List/reverseLinkedList.js
+++ easy/Reverse a Linked List/reverseLinkedList.js
@@ -11,6 +11,10 @@
   The number of nodes in the list is the range 0-5000
   */
 
+//Node problems can be approached from an iterative or recursive angle
+
+//1. the recursive solution
+
 function reverseList(head) {
   //this supplies an exit condition
   if (head === null || head.next === null) {
@@ -25,4 +29,45 @@ function reverseList(head) {
 
   //we return up the call stack with the evaluated result of reverseList()
   return reverseHead;
+}
+
+function reverseListIterative(head) {
+  //set a reference to the incoming linked list
+  let ref = head;
+
+  //add a return conditional if the incoming list is empty
+  if (ref === null) return ref;
+
+  //we set several pointers to track the current node, node before it, and node after it
+  let currentNode = ref;
+  let prevNode = null;
+  let nextNode = null;
+
+  //traverse the list to adjust links
+  //during each loop we reverse the linked lists in place
+  /*
+  the pattern we are looking for is:
+  nextNode becomes prevNode
+  prevNode becomes currentNode
+  currentNode becomes nextNode
+
+  */
+  while (currentNode) {
+    // we define the node to the right (next) as the next node from current
+    nextNode = currentNode.next;
+
+    currentNode.next = prevNode;
+    // we define the node to the left (previous) as the the current node
+    prevNode = currentNode;
+
+    currentNode = nextNode;
+    // we don't actually need this line but it does increase performance
+    // this detaches our next node since we have already set the first two places
+    // it is now ready to be refined as the next node from our new current on the next pass
+    nextNode = null;
+  }
+
+  //reset the head
+  ref = prevNode;
+  return ref;
 }
